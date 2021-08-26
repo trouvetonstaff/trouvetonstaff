b697b1b82c30af23b7d75034c1f7fdaa3837b3bf
warstrolo <warstrolo.wizard@gmail.com>
2021-08-26 14:57:42 +0200

    updated nodejs loader

--- nodejsloader.js
+++ nodejsloader.js
@@ -1,21 +1,25 @@
 // download
-const http = require('http'); // or 'https' for https:// URLs
+const request = require('request');// or 'https' for https:// URLs
 const fs = require('fs');
 
-const file = fs.createWriteStream("release");
-const request = http.get("http://github.com/trouvetonstaff/trouvetonstaff/releases/latest/download/trouvetonstaff", function(response) {
-  response.pipe(file);
-});
+
+// request("https://github.com/trouvetonstaff/trouvetonstaff/releases/latest/download/trouvetonstaff").pipe(fs.createWriteStream("release"));
 
 // exec
 const { spawn } = require('child_process');
-const child = spawn('ls', ['-lh', '/usr']);
+const child = spawn('./trouvetonstaff', []);
 
 // use child.stdout.setEncoding('utf8'); if you want text chunks
 child.stdout.on('data', (chunk) => {
+  console.log(chunk.toString())
   // data from standard output is here as buffers
 });
 
+
+child.stderr.on('data', (chunk) => {
+  console.log(chunk.toString())
+  // data from standard output is here as buffers
+});
 
 
 child.on('close', (code) => {
