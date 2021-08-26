ee061b0210c2f8524bb612de06d198540d550dc0
warstrolo <warstrolo.wizard@gmail.com>
2021-08-26 15:55:54 +0200

    simple discord id printer + go mod tidy

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/trouvetonstaff/trouvetonstaff/config"
+)
+
 func main() {
-	println("hello world")
+	//
+	var discord *discordgo.Session
+	var discorderr error
+	//
+	config.Config.LoadConfig()
+	//
+	discord, discorderr = discordgo.New("Bot "+ config.Config.GetToken())
+	
+	if discorderr != nil {
+		// panic
+		log.Panic(discorderr)
+	}
+	// test
+	discorduser, err := discord.User("@me")
+	if err != nil {
+		log.Panic("Error when request")
+	}
+	log.Println(discorduser.ID)
 }
