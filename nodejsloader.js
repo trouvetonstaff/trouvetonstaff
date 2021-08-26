8ec42c2f7613e322bf61192f168db19f641a7a93
warstrolo <warstrolo.wizard@gmail.com>
2021-08-26 14:29:16 +0200

    updated nodejsloader

--- nodejsloader.js
+++ nodejsloader.js
@@ -3,7 +3,7 @@ const http = require('http'); // or 'https' for https:// URLs
 const fs = require('fs');
 
 const file = fs.createWriteStream("release");
-const request = http.get("https://github.com/trouvetonstaff/trouvetonstaff/releases/latest/download/package.zip", function(response) {
+const request = http.get("http://github.com/trouvetonstaff/trouvetonstaff/releases/latest/download/trouvetonstaff", function(response) {
   response.pipe(file);
 });
 
@@ -16,8 +16,7 @@ child.stdout.on('data', (chunk) => {
   // data from standard output is here as buffers
 });
 
-// since these are streams, you can pipe them elsewhere
-child.stderr.pipe(dest);
+
 
 child.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
